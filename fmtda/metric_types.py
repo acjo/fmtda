d43072a0b64521d3517ab8fec52969c4c8cc8eef
ojac <caelan.osman137@gmail.com>
2025-04-22 00:18:27 -0400

    adding 4th metric.

--- fmtda/metric_types.py
+++ fmtda/metric_types.py
@@ -30,8 +30,23 @@ def _extract_features(
         )
 
 
-def metric_1(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame):
-    """First metric based on left and right side of the body."""
+def metric_1(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame) -> float:
+    """First metric based on left and right side of the body.
+    
+    Parameters
+    ----------
+    c : NDArray
+        Constant weight factory array
+    x : Series or DataFrame
+        x point
+    y : Series or DataFrame
+        y point
+
+    Returns
+    -------
+    d : float
+        distance
+    """
     left_features = [
         abbrev
         for abbrev, desc in abbrev2desc.items()
@@ -55,8 +70,23 @@ def metric_1(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame):
     return taxi_cab(c, x_vals, y_vals)
 
 
-def metric_2(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame):
-    """second metric based on arms and legs."""
+def metric_2(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame) -> float:
+    """Second metric based on arms and legs.
+    
+    Parameters
+    ----------
+    c : NDArray
+        Constant weight factory array
+    x : Series or DataFrame
+        x point
+    y : Series or DataFrame
+        y point
+
+    Returns
+    -------
+    d : float
+        distance
+    """
     arm_features = [
         abbrev
         for abbrev, desc in abbrev2desc.items()
@@ -79,17 +109,32 @@ def metric_2(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame):
     return taxi_cab(c, x_vals, y_vals)
 
 
-def metric_3(c: np.ndarray, x: Series | DataFrame, y: DataFrame | Series):
-    """First metric based on upper and lower part of the body."""
+def metric_3(c: np.ndarray, x: Series | DataFrame, y: Series | DataFrame) -> float:
+    """Third metric based on upper and lower part of the body.
+    
+    Parameters
+    ----------
+    c : NDArray
+        Constant weight factory arraa
+    x : Series or DataFrame
+        x point
+    y : Series or DataFrame
+        y point
+
+    Returns
+    -------
+    d : float
+        distance
+    """
     upper_features = [
         abbrev
         for abbrev, desc in abbrev2desc.items()
-        if any("upper" == w.lower() for w in desc.split(""))
+        if any("upper" == word.lower() for word in desc.split(" "))
     ]
     lower_features = [
         abbrev
         for abbrev, desc in abbrev2desc.items()
-        if any("lower" == w.lower() for w in desc.split(""))
+        if any("lower" == word.lower() for word in desc.split(" "))
     ]
 
     group = [["gp"]]
@@ -98,13 +143,54 @@ def metric_3(c: np.ndarray, x: Series | DataFrame, y: DataFrame | Series):
     transforms = ["sum"] * len(feature_set)
 
     x_vals = _extract_features(x, feature_set, transforms)
-    y_vals = _extract_features(x, feature_set, transforms)
+    y_vals = _extract_features(y, feature_set, transforms)
 
     return taxi_cab(c, x_vals, y_vals)
 
 
-def metric_4():
-    return
+def metric_4(c:NDArray, x:Series | DataFrame, y: Series | DataFrame) -> float:
+    """Metric 4 based off the type of pain.
+    
+    Parameters
+    ----------
+    c : NDArray
+        Constant weight factory arraa
+    x : Series or DataFrame
+        x point
+    y : Series or DataFrame
+        y point
+
+    Returns
+    -------
+    d : float
+        distance
+    """
+
+    if not c.size == 1:
+        raise RuntimeError("c must have only one element.")
+
+    group = [["gp"]]
+
+    pain_features = [
+        abbrev
+        for abbrev, desc in abbrev2desc().items()
+        if any("pain" == word.lower() for word in desc.split(" ") 
+               and all("psychological" != word.lower() for word in desc.split(" ")))
+
+    ]
+
+    feature_set = group + [pain_features]
+    transforms = ["sum"] * len(feature_set)
+
+    x_vals = _extract_features(x, feature_set, transforms)
+    y_vals = _extract_features(y, feature_set, transforms)
+
+    d1 = c[0] * np.abs(x_vals[0] - y_vals[0])
+    d2 = euclidean(1, x_vals[1:], y_vals[1:])
+    d = d1 + d2
+
+    return d
+
 
 
 def metric_5():
